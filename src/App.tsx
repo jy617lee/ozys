69ec7fee264037fc96b06dbb4272761feabaeab8
june <jy617.lee@gmail.com>
2023-05-16 00:01:58 +0900

    feat: show popups on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import TokenDropdown from "./components/TokenDropdown";
 import { TOKENS } from "./data/tokens";
 import { TOKEN } from "./model/token";
 import styled from "styled-components";
+import { usePopup } from "./hooks/usePopup";
+import PopupA from "./components/popup/PopupA";
 
 function App() {
   const [selectedToken, setSelectedToken] = useState<TOKEN>();
+
+  const { popups, openPopup, closePopup } = usePopup();
 
   return (
     <div className='App'>
@@ -32,6 +36,18 @@ function App() {
 
       <Container>
         <Title>2. 모달</Title>
+        <OpenModalButton
+          onClick={() => {
+            openPopup(<PopupA />);
+          }}
+        >
+          Open Modal A
+        </OpenModalButton>
+
+        {popups.length > 0 && <ModalOverlay onClick={closePopup} />}
+        {popups.map((popup, index) => (
+          <div key={index}>{popup}</div>
+        ))}
       </Container>
     </div>
   );
@@ -64,4 +80,21 @@ const TokenImage = styled.img`
   margin-right: 5px;
   border-radius: 50%;
 `;
+
+const OpenModalButton = styled.button`
+  width: 200px;
+  height: 50px;
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: bold;
+`;
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.8);
+`;
+
 export default App;
